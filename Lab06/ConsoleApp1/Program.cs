6fe31bdb96168ac6946ea47a96446390bb924780
Klar-Bro <klara.broszkiewicz@gmail.com>
2024-12-03 16:58:50 +0100

    Add files via upload

---
+++ Lab06/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+
+Person child = Person.NewlyBornChild;
+child.FirstName = "Brian";
+child.LastName = "Doe";
+
+Person p1 = new Person();
+p1.FirstName = "Jane";
+p1.LastName = "Doe";
+
+Person p2 = new Person();
+p2.FirstName = "Eryko";
+p2.LastName = "Meryko";
+
+Person.Relate(p1, p2);
+
+
+
