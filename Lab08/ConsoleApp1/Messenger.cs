b3fcc6de68d243f4b581ddfdfe3cdbf7b71a6253
Klar-Bro <klara.broszkiewicz@gmail.com>
2024-12-10 17:51:46 +0100

    Add files via upload

---
+++ Lab08/ConsoleApp1/Messenger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Messenger<T> where T : IMessage
+    {
+        public void Send(T message)
+        {
+            Console.WriteLine($"Sending {message.GetType().Name}: {message.Content}");
+        }
+    }
+}
---
+++ Lab08/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var letter = new Letter { Content = "Secret content of the letter." };
+        var email = new Email { Content = "Important email message." };
+        var sms = new SMS { Content = "Quick SMS notification." };
+
+        var letterMessenger = new Messenger<Letter>();
+        var emailMessenger = new Messenger<Email>();
+        var smsMessenger = new Messenger<SMS>();
+
+        letterMessenger.Send(letter); // Sending Letter: Secret content of the letter.
+        emailMessenger.Send(email);   // Sending Email: Important email message.
+        smsMessenger.Send(sms);       // Sending SMS: Quick SMS notification.
+    }
+}
